ac0f929d45518cf091ff81774383b50b1b07cf9d
anjanybeauty-crypto <anjanybeauty6f22@gmail.com>
2026-05-31 20:00:46 +0700

    Update welcome.js

--- login/js/welcome.js
+++ login/js/welcome.js
@@ -1,25 +1,17 @@
+function goLogin() {
+    window.location.href = "login/index.html";
+}
+
+function logout() {
+    localStorage.removeItem("username");
+    location.reload();
+}
+
 document.addEventListener("DOMContentLoaded", function () {
-
-    function goLogin() {
-        window.location.href = "login/index.html";
-    }
-
-    function logout() {
-        localStorage.removeItem("username");
-        location.reload();
-    }
-
     const user = localStorage.getItem("username");
 
     if (user) {
         document.getElementById("userInfo").innerText = "Halo, " + user;
-
-        document.getElementById("authArea").innerHTML = `
-            <button onclick="logout()" class="nav-cta">Logout</button>
-        `;
+        document.getElementById("authArea").innerHTML = '<button onclick="logout()" class="nav-cta">Logout</button>';
     }
-
-    window.goLogin = goLogin;
-    window.logout = logout;
-
 });
