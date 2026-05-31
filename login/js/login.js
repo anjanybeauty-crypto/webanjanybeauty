4d4b11abfd9476d327befdead794b0991135a5b1
anjanybeauty-crypto <anjanybeauty6f22@gmail.com>
2026-05-31 20:08:13 +0700

    Update login.js

--- login/js/login.js
+++ login/js/login.js
@@ -1,7 +1,7 @@
 document.getElementById("loginForm").addEventListener("submit", async function(e) {
     e.preventDefault();
 
-    const username = document.getElementById("username").value.trim();
+    const email = document.getElementById("email").value.trim();
     const password = document.getElementById("password").value.trim();
 
     const res = await fetch("https://herisusanta.my.id/javalogin/api/auth.php", {
@@ -9,28 +9,24 @@ document.getElementById("loginForm").addEventListener("submit", async function(e
         headers: {
             "Content-Type": "application/x-www-form-urlencoded"
         },
-        body: `action=login&username=${encodeURIComponent(username)}&password=${encodeURIComponent(password)}`
+        body: `action=login&username=${encodeURIComponent(email)}&password=${encodeURIComponent(password)}`
     });
 
     const data = await res.json();
 
     if (data.status === "success") {
-        // simpan username
-            localStorage.setItem("username", data.username);
-            window.location.href = "../index.html";
-         
-    // } else {
-    //     document.getElementById("message").innerText = "Username / Password salah";alert("Login gagal");
-    // }
-    
+        localStorage.setItem("username", data.username);
+        window.location.href = "../index.html";
     } else {
-    const alertBox = document.getElementById("alertBox");
-    alertBox.innerText = "Username atau Password salah, silahkan coba lagi";
-    alertBox.style.display = "block";
-
-    setTimeout(() => {
-        alertBox.style.display = "none";
-    }, 3000);
-} 
-   
+        const alertBox = document.getElementById("alertBox");
+        if (alertBox) {
+            alertBox.innerText = "Username atau Password salah, silahkan coba lagi";
+            alertBox.style.display = "block";
+            setTimeout(() => {
+                alertBox.style.display = "none";
+            }, 3000);
+        } else {
+            alert("Username atau Password salah!");
+        }
+    }
 });
